48fe4a194730b7d989cf5d48552cecb83a1af844
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-06-26 15:59:55 +0000

    LandingPage - add a button for booking a course and styling

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -13,8 +13,11 @@ const LandingPage = () => {
                     <h1>Welcome to Diving Center</h1>
                     <p>Come live out your ideal diving vacation with us</p>
                 </div>
-            <Link to="/signin" className={styles['home-btn']}>Explore</Link>
-        </div>
+                <div className={styles['button-container']}>
+                    <Link to="/signin" className={styles['home-btn']}>Explore</Link>
+                    <Link to="/booking/create" className={styles['home-btn']}>Book a Course</Link>
+                </div>
+            </div>
         </div>
     );
 }
