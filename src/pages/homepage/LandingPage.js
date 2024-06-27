e351aae2f4865f54449e29076f6b52c5078486b3
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-06-27 12:50:35 +0000

    LangingPage - add new description to the home page

--- src/pages/homepage/LandingPage.js
+++ src/pages/homepage/LandingPage.js
@@ -10,12 +10,13 @@ const LandingPage = () => {
             <div className={styles['bg-overlay']}></div>
             <div className={styles['content-wrapper']}>
                 <div className={styles['home-text']}>
-                    <h1>Welcome to Diving Center</h1>
-                    <p>Come live out your ideal diving vacation with us</p>
+                    <h1>From Beginner Bubbles to Advanced Adventures.</h1>
+                    <p>No matter your experience level, we have something for you. </p>
+                    <p>Join our inclusive community or explore our diving courses designed for all skill sets.</p>
                 </div>
                 <div className={styles['button-container']}>
-                    <Link to="/signin" className={styles['home-btn']}>Explore</Link>
-                    <Link to="/bookings/create" className={styles['home-btn']}>Book a Course</Link>
+                    <Link to="/signin" className={styles['home-btn']}>Diving Community</Link>
+                    <Link to="/courses" className={styles['home-btn']}>Diving Courses</Link>
                 </div>
             </div>
         </div>
