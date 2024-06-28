b4c9466b3e72422cc708299d690a3cb977cf2fc2
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-06-28 15:26:19 +0000

    create Footer.js and ad it to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
@@ -22,74 +23,73 @@ import NotFound from "./components/NotFound";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 function App() {
   const currentUser = useCurrentUser();
   const profile_id = currentUser?.profile_id || "";
 
   return (
-    <>
     <div className={styles.App}>
       <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/bookings/create" component={BookingForm} />
-        <Route exact path="/courses" component={CoursesPage} />
-          <Route
-            exact
-            path="/posts"
-            render={() => (
-              <PostsPage message="No results found. Adjust the search keyword." />
-            )}
-          />
-          <Route
-            exact
-            path="/feed"
-            render={() => (
-              <PostsPage
-                message="No results found. Adjust the search keyword or follow a user."
-                filter={`owner__followed__owner__profile=${profile_id}&`}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/liked"
-            render={() => (
-              <PostsPage
-                message="No results found. Adjust the search keyword or like a post."
-                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
-              />
-            )}
-          />
-          <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
-          <Route exact path="/posts/:id" render={() => <PostPage />} />
-          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
-          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
-          <Route
-            exact
-            path="/profiles/:id/edit/username"
-            render={() => <UsernameForm />}
-          />
-          <Route
-            exact
-            path="/profiles/:id/edit/password"
-            render={() => <UserPasswordForm />}
-          />
-          <Route
-            exact
-            path="/profiles/:id/edit"
-            render={() => <ProfileEditForm />}
-          />
-
-          <Route render={() => <NotFound />} />
-        </Switch>
-      </Container>
-    </div>
-    <ToastContainer
+      <div className={styles.MainWrapper}>
+        <Container className={styles.Main}>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/bookings/create" component={BookingForm} />
+            <Route exact path="/courses" component={CoursesPage} />
+            <Route
+              exact
+              path="/posts"
+              render={() => (
+                <PostsPage message="No results found. Adjust the search keyword." />
+              )}
+            />
+            <Route
+              exact
+              path="/feed"
+              render={() => (
+                <PostsPage
+                  message="No results found. Adjust the search keyword or follow a user."
+                  filter={`owner__followed__owner__profile=${profile_id}&`}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/liked"
+              render={() => (
+                <PostsPage
+                  message="No results found. Adjust the search keyword or like a post."
+                  filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+                />
+              )}
+            />
+            <Route exact path="/signin" render={() => <SignInForm />} />
+            <Route exact path="/signup" render={() => <SignUpForm />} />
+            <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+            <Route exact path="/posts/:id" render={() => <PostPage />} />
+            <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+            <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
+            <Route
+              exact
+              path="/profiles/:id/edit/username"
+              render={() => <UsernameForm />}
+            />
+            <Route
+              exact
+              path="/profiles/:id/edit/password"
+              render={() => <UserPasswordForm />}
+            />
+            <Route
+              exact
+              path="/profiles/:id/edit"
+              render={() => <ProfileEditForm />}
+            />
+            <Route render={() => <NotFound />} />
+          </Switch>
+        </Container>
+      </div>
+      <Footer />
+      <ToastContainer
         position="top-center"
         autoClose={5000}
         hideProgressBar={false}
@@ -103,7 +103,7 @@ function App() {
         toastClassName={styles.customToast}
         style={{ zIndex: 9999 }}
       />
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from '../styles/Footer.module.css';
+
+const Footer = ({ className }) => {
+  const currentYear = new Date().getFullYear();
+
+  const socialLinks = [
+    { href: "https://github.com/AmirShkolnik", icon: "fab fa-github", label: "GitHub" },
+    { href: "https://www.linkedin.com/in/amirshkolnik/", icon: "fab fa-linkedin", label: "LinkedIn" }
+  ];
+
+  return (
+    <footer className={`${styles.footer} ${className}`}>
+      <p>&copy; {currentYear} Amir Shkolnik. All rights reserved.</p>
+      <div className={styles['footer-icons']}>
+        {socialLinks.map((link, index) => (
+          <a 
+            key={index}
+            href={link.href} 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            aria-label={link.label}
+          >
+            <i className={link.icon}></i>
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
