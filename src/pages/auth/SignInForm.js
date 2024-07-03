2c014600d1d88211543b04f6b642a68750a2afef
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-07-03 14:55:53 +0000

    signinform - add toastify messages and redirect user to his profile after signing in

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -13,6 +13,7 @@ import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
 import VideoPlayerSignIn from '../../components/Video/VideoPlayerSignIn.js';
+import { toast } from 'react-toastify';
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
@@ -33,9 +34,11 @@ function SignInForm() {
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
-      history.goBack();
+      toast.success("Successfully signed in!");
+      history.push(`/profiles/${data.user.profile_id}`);
     } catch (err) {
       setErrors(err.response?.data);
+      toast.error("Sign in failed. Please check your credentials.");
     }
   };
 
