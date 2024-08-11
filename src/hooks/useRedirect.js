830e51cdc3ab31de93f3c7c700e066500128bc03
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-08-11 14:05:10 +0000

    useRedirect - improve user redirection logic in useRedirect hook

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -1,26 +1,25 @@
-import axios from 'axios';
 import { useEffect } from 'react';
-import { useHistory } from 'react-router';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 export const useRedirect = (userAuthStatus) => {
   const history = useHistory();
 
   useEffect(() => {
-    const handleMount = async () => {
+    const handleRedirect = async () => {
       try {
         await axios.post('/dj-rest-auth/token/refresh/');
-        // if user is logged in, the code below will run
+
         if (userAuthStatus === 'loggedIn') {
-          history.push('/');
+          return;
         }
       } catch (err) {
-        // if user is not logged in, the code below will run
         if (userAuthStatus === 'loggedOut') {
-          history.push('/');
+          history.push('/login');
         }
       }
     };
 
-    handleMount();
+    handleRedirect();
   }, [history, userAuthStatus]);
 };
